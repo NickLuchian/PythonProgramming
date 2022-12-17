b7647d1546765441b91dd8288a15c6beccfa606b
Nick Luchian <nicolai.luchian@gmail.com>
2022-12-17 15:27:18 +0200

    Python Basics

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Nico')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+#first it will check if __name__ == __main__ (in this case module name is variables from variables.py)
+import variables
---
+++ variables.py
@@ -0,0 +1,72 @@
+def main():
+    # ---------------------------------Variables name are key sensitive
+    # String
+    name = 'python'
+    # int
+    age = 30
+    # List
+    listOfThings = ['ball', 'car', 'plane']
+
+    # ------------------------------Get the python keywords
+    # import keyword
+    # kw_List = keyword.kwlist
+    #
+    # for i in kw_List:
+    #     print(i)
+
+    # T--------------------------------------ypes
+    # int, float, double
+
+    # -------------------------------------------finding of min/max values
+    # minValue = min(-1,5,-456,4,7846)
+    # maxValue = max(-1,5,-456,4,7846)
+    # print(maxValue, minValue)
+
+    # -------------------------------------------String methods
+    my_string = "Python is awesome"
+    my_string = my_string.lower()
+    my_string = my_string.split(" ")
+    my_string = "-".join(my_string)
+    # print(my_string)
+
+    """this is how to use f-strings"""
+    # print(f"Person name is: {name} and age {age}")
+
+    """this is how to iterate using [indexes]"""
+    # print(len(my_string))
+    #
+    # print(my_string[-1])
+    # #is the same as
+    # print(my_string[len(my_string)-1])
+
+    # print(my_string[0:6])
+    test_number = '1234567894844343543843543843548435'
+    # print(test_number[::2])  # -----------------------this is how to slice the string using defined step
+    # # this is how to iterate a string backwards
+    # print(test_number[::-1])
+    # 'qwerty'[::-1]
+
+    # # -------------------------------------this is how to iterate a number backwards (parse a number to a string using str() function)
+    # num = 51543543453
+    # u = str(num)
+    # print(u[::-1])
+    #
+    # for i in reversed(test_number):
+    #     print(i)
+
+    # -----------------------------------------------------input function
+    """all the input type values are treated as strings"""
+    name1 = input('Введите имя: ')
+    print(name1)
+    """to convert input directly to int use int() function"""
+    age1 = int(input('Введите возраст: '))
+    print(type(age1))
+
+    # ---------------------------------------------------import mudule example
+    # print(f'variables module name is: {__name__}')
+    """in case that we define name == main we can control modules importing"""
+if __name__ == '__main__':
+    print(f'this is the message if name == main')
+    main()
+else:
+    print(f'sorry dude, name is not main')
